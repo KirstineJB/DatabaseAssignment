9c082992a9723ea2b3d04412f7d8eb5e04e46656
KirstineJB <kirstine.berantzino@gmail.com>
2025-10-03 14:28:51 +0200

    - Added instructor class with relation to Course

---
+++ StudentManagement/Models/Instructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; } // PK
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime HireDate { get; set; }
+
+
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+    }
+}
