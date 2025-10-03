dd2da72a3f5daa690f5ffe6abccf527871d6ff25
KirstineJB <kirstine.berantzino@gmail.com>
2025-10-03 16:08:42 +0200

    Statebased: Modify CourseCredit

---
+++ StudentManagement/db-scripts/StateBased_v7.sql
@@ -0,0 +1,292 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003084551_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Courses] (
+        [Id] int NOT NULL IDENTITY,
+        [Title] nvarchar(max) NOT NULL,
+        [Credits] int NOT NULL,
+        CONSTRAINT [PK_Courses] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003084551_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Students] (
+        [Id] int NOT NULL IDENTITY,
+        [FirstName] nvarchar(max) NOT NULL,
+        [LastName] nvarchar(max) NOT NULL,
+        [Email] nvarchar(max) NOT NULL,
+        [EnrollmentDate] datetime2 NOT NULL,
+        CONSTRAINT [PK_Students] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003084551_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Enrollments] (
+        [Id] int NOT NULL IDENTITY,
+        [StudentId] int NOT NULL,
+        [CourseId] int NOT NULL,
+        [Grade] nvarchar(max) NULL,
+        CONSTRAINT [PK_Enrollments] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Enrollments_Courses_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Courses] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Enrollments_Students_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Students] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003084551_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_Enrollments_CourseId] ON [Enrollments] ([CourseId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003084551_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_Enrollments_StudentId] ON [Enrollments] ([StudentId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003084551_InitialCreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251003084551_InitialCreate', N'9.0.9');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003092429_AddMiddleNameToStudent'
+)
+BEGIN
+    ALTER TABLE [Students] ADD [MiddleName] nvarchar(max) NOT NULL DEFAULT N'';
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003092429_AddMiddleNameToStudent'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251003092429_AddMiddleNameToStudent', N'9.0.9');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003120205_AddStudentMiddleName'
+)
+BEGIN
+    DECLARE @var sysname;
+    SELECT @var = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Students]') AND [c].[name] = N'MiddleName');
+    IF @var IS NOT NULL EXEC(N'ALTER TABLE [Students] DROP CONSTRAINT [' + @var + '];');
+    ALTER TABLE [Students] ALTER COLUMN [MiddleName] nvarchar(max) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003120205_AddStudentMiddleName'
+)
+BEGIN
+    ALTER TABLE [Students] ADD [DateOfBirth] datetime2 NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003120205_AddStudentMiddleName'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251003120205_AddStudentMiddleName', N'9.0.9');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003121813_AddInstructorAndCourse'
+)
+BEGIN
+    ALTER TABLE [Courses] ADD [InstructorId] int NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003121813_AddInstructorAndCourse'
+)
+BEGIN
+    CREATE TABLE [Instructor] (
+        [Id] int NOT NULL IDENTITY,
+        [FirstName] nvarchar(max) NOT NULL,
+        [LastName] nvarchar(max) NOT NULL,
+        [Email] nvarchar(max) NOT NULL,
+        [HireDate] datetime2 NOT NULL,
+        CONSTRAINT [PK_Instructor] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003121813_AddInstructorAndCourse'
+)
+BEGIN
+    CREATE INDEX [IX_Courses_InstructorId] ON [Courses] ([InstructorId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003121813_AddInstructorAndCourse'
+)
+BEGIN
+    ALTER TABLE [Courses] ADD CONSTRAINT [FK_Courses_Instructor_InstructorId] FOREIGN KEY ([InstructorId]) REFERENCES [Instructor] ([Id]) ON DELETE SET NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003121813_AddInstructorAndCourse'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251003121813_AddInstructorAndCourse', N'9.0.9');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003124816_RenameEnrollmentGradeToFinalGrade'
+)
+BEGIN
+    EXEC sp_rename N'[Enrollments].[Grade]', N'FinalGrade', 'COLUMN';
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003124816_RenameEnrollmentGradeToFinalGrade'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251003124816_RenameEnrollmentGradeToFinalGrade', N'9.0.9');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003134304_AddDepartments'
+)
+BEGIN
+    ALTER TABLE [Courses] DROP CONSTRAINT [FK_Courses_Instructor_InstructorId];
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003134304_AddDepartments'
+)
+BEGIN
+    ALTER TABLE [Instructor] DROP CONSTRAINT [PK_Instructor];
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003134304_AddDepartments'
+)
+BEGIN
+    EXEC sp_rename N'[Instructor]', N'Instructors', 'OBJECT';
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003134304_AddDepartments'
+)
+BEGIN
+    ALTER TABLE [Instructors] ADD CONSTRAINT [PK_Instructors] PRIMARY KEY ([Id]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003134304_AddDepartments'
+)
+BEGIN
+    CREATE TABLE [Departments] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(100) NOT NULL,
+        [Budget] decimal(18,2) NOT NULL,
+        [StartDate] date NOT NULL,
+        [DepartmentHeadId] int NULL,
+        CONSTRAINT [PK_Departments] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Departments_Instructors_DepartmentHeadId] FOREIGN KEY ([DepartmentHeadId]) REFERENCES [Instructors] ([Id]) ON DELETE SET NULL
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003134304_AddDepartments'
+)
+BEGIN
+    CREATE INDEX [IX_Departments_DepartmentHeadId] ON [Departments] ([DepartmentHeadId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003134304_AddDepartments'
+)
+BEGIN
+    ALTER TABLE [Courses] ADD CONSTRAINT [FK_Courses_Instructors_InstructorId] FOREIGN KEY ([InstructorId]) REFERENCES [Instructors] ([Id]) ON DELETE SET NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003134304_AddDepartments'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251003134304_AddDepartments', N'9.0.9');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003140001_ModifyCourseCredits'
+)
+BEGIN
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Courses]') AND [c].[name] = N'Credits');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Courses] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [Courses] ALTER COLUMN [Credits] decimal(5,2) NOT NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003140001_ModifyCourseCredits'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251003140001_ModifyCourseCredits', N'9.0.9');
+END;
+
+COMMIT;
+GO
+
